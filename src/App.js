eca3521301cb04cad373a42d53ee2348c945b5fb
Bill Cathey <billcathey@hotmail.com>
2021-06-07 15:24:46 -0400

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+import React, { useEffect } from "react";
+
 function App() {
+  // const [images, setImages] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  const currentDate = new Date();
+  const earlierDate = currentDate.setMonth(currentDate.getMonth() - 1);
+  const startDate = new Date(earlierDate).toISOString().slice(0, 10);
+  console.log(startDate);
+
+  useEffect(() => {
+    fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&start_date=${startDate}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <h1 className="text-6xl">Hello World!</h1>
