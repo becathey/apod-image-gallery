2e990b40c0489f4e80818421d15a9990595a9d46
Bill Cathey <billcathey@hotmail.com>
2021-06-07 20:52:24 -0400

    Add state, set data to state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  // const [images, setImages] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const currentDate = new Date();
   const earlierDate = currentDate.setMonth(currentDate.getMonth() - 1);
   const startDate = new Date(earlierDate).toISOString().slice(0, 10);
-  console.log(startDate);
 
   useEffect(() => {
     fetch(
       `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&start_date=${startDate}`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        setImages(data);
+        setIsLoading(false);
+      })
       .catch((err) => console.log(err));
   }, []);
   return (
