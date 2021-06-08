7ca569b22996652b176f68c7f448304af9ce8b9b
Bill Cathey <billcathey@hotmail.com>
2021-06-07 22:36:39 -0400

    Edit styles for image cards

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,10 +5,12 @@ const ImageCard = ({ image }) => {
     <div className="max-w-sm rounded overflow-hidden shadow-lg mb-3">
       <img src={image.url} alt={image.title} className="w-full" />
       <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-lg mb-2">
+        <div className="font-bold text-purple-500 text-center text-lg mb-2">
           {image.date}
         </div>
-        <div className="font-semibold text-500 text-lg mb-2">{image.title}</div>
+        <div className="font-semibold text-500 text-center text-lg mb-2">
+          {image.title}
+        </div>
       </div>
     </div>
   );
